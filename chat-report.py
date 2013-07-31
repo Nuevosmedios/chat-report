73deb1d5540d61e150577e88212d7935407d9648
Gustavo A. Gómez Farhat <gustavo.gomez.farhat@gmail.com>
2013-07-31 15:30:43 -0500

    Trying some things

--- chat-report.py
+++ chat-report.py
@@ -1,15 +1,24 @@
+#import unicodecsv
+#from unicodewriter import UnicodeWriter
+#from cStringIO import StringIO
 from flask import Flask, Response, request
 from models import MessageArchive
 
 app = Flask(__name__)
 
+
 @app.route("/reporte.csv")
 def report():
     chatroom = request.args.get('chatroom')
+
     def generate():
-        data = MessageArchive.query.filter(MessageArchive.toJID.like(chatroom)).order_by(MessageArchive.sentDate).all()
+        #f = StringIO()
+        #w = unicodecsv.writer(f, encoding='utf-8')
+        data = MessageArchive.query.filter(MessageArchive.toJID.like(chatroom)).all()
         for d in data:
-            yield ','.join([str(d.sentDate), d.toJIDResource, d.body, '\n'])
+            #w.writerow([str(d.sentDate), d.toJIDResource, d.body, '\n'])
+            yield ','.join([str(d.sentDate), d.toJIDResource, d.body]) + '\n'
+        #return Response(response=f, mimetype='text/csv')
     return Response(generate(), mimetype='text/csv')
 
 if __name__ == "__main__":
