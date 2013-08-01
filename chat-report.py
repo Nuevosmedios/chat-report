92fb7d674361145ef5ae0964aea6886a650d29bb
Gustavo A. Gómez Farhat <gustavo.gomez.farhat@gmail.com>
2013-08-01 12:08:14 -0500

    Finished!

--- chat-report.py
+++ chat-report.py
@@ -1,6 +1,5 @@
-#import unicodecsv
-#from unicodewriter import UnicodeWriter
-#from cStringIO import StringIO
+from datetime import datetime
+import time
 from flask import Flask, Response, request
 from models import MessageArchive
 
@@ -10,15 +9,19 @@ app = Flask(__name__)
 @app.route("/reporte.csv")
 def report():
     chatroom = request.args.get('chatroom')
+    begin_date = request.args.get('begin_date')
+    end_date = request.args.get('end_date')
+    begin_datetime = datetime.strptime(begin_date, '%Y-%m-%d')
+    end_datetime = datetime.strptime(end_date, '%Y-%m-%d')
+    begin_timestamp = time.mktime(begin_datetime.timetuple()) * 1e3
+    end_timestamp = time.mktime(end_datetime.timetuple()) * 1e3
 
     def generate():
-        #f = StringIO()
-        #w = unicodecsv.writer(f, encoding='utf-8')
-        data = MessageArchive.query.filter(MessageArchive.toJID.like(chatroom)).all()
+        data = MessageArchive.query.filter(MessageArchive.toJID.like(chatroom))
+        data = data.filter(MessageArchive.sentDate.between(begin_timestamp, end_timestamp)).all()
         for d in data:
-            #w.writerow([str(d.sentDate), d.toJIDResource, d.body, '\n'])
-            yield ','.join([str(d.sentDate), d.toJIDResource, d.body]) + '\n'
-        #return Response(response=f, mimetype='text/csv')
+            date = datetime.fromtimestamp(d.sentDate / 1e3)
+            yield ','.join([str(date), d.toJIDResource, d.body]) + '\n'
     return Response(generate(), mimetype='text/csv')
 
 if __name__ == "__main__":
