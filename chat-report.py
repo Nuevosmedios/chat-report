ee3d821b2b02ce808493621c3bf9f20aae2108a6
Gustavo Gómez <gustavo.gomez@nuevosmedios.ws>
2014-02-01 19:27:54 -0500

    PDF report

--- chat-report.py
+++ chat-report.py
@@ -7,26 +7,6 @@ from models import MessageArchive
 app = Flask(__name__)
 
 
-# @app.route("/reporte.csv")
-# def report():
-#     chatroom = request.args.get('chatroom')
-#     begin_date = request.args.get('begin_date')
-#     end_date = request.args.get('end_date')
-#     begin_datetime = datetime.strptime(begin_date, '%Y-%m-%d')
-#     end_datetime = datetime.strptime(end_date, '%Y-%m-%d')
-#     begin_timestamp = time.mktime(begin_datetime.timetuple()) * 1e3
-#     end_timestamp = time.mktime(end_datetime.timetuple()) * 1e3
-
-#     def generate():
-#         data = MessageArchive.query.filter(MessageArchive.toJID.like(chatroom))
-#         data = data.filter(MessageArchive.sentDate.between(begin_timestamp, end_timestamp)).all()
-#         for d in data:
-#             print type(d.toJIDResource)
-#             date = datetime.fromtimestamp(d.sentDate / 1e3)
-#             yield ','.join([str(date), d.toJIDResource, d.body]) + '\n'
-#     return Response(generate(), mimetype='text/csv')
-
-
 @app.route("/reporte/")
 def report_to_html():
     chatroom = request.args.get('chatroom')
@@ -42,16 +22,21 @@ def report_to_html():
     objects = []
     for d in data:
         date = datetime.fromtimestamp(d.sentDate / 1e3)
-        objects.append((str(date),
+        objects.append((str(date)[:-7],
                         d.toJIDResource.replace('\\20', ' ').replace('\\40', '@'),
                         d.body.replace('\n', '').replace('|c:3|', ''),))
     return render_template('report.html', objects=objects)
 
 
-@app.route('/reporte.pdf')
-def hello_pdf():
-    # Make a PDF from another view
-    return render_pdf(url_for('hello_html', name=name))
-
+@app.route('/reporte.csv/')
+def report_to_pdf():
+    chatroom = request.args.get('chatroom')
+    begin_date = request.args.get('begin_date')
+    end_date = request.args.get('end_date')
+    
+    return render_pdf(url_for('report_to_html',
+                              chatroom=chatroom,
+                              begin_date=begin_date,
+                              end_date=end_date))
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
